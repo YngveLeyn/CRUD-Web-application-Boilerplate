ac8134115f4a860a782bb1113b62d26bb7c2211a
Yngve Leyn <yngve.leyn@gmail.com>
2017-03-18 23:39:11 +0100

    Cleaned up read.php

--- pages/read.php
+++ pages/read.php
@@ -2,37 +2,30 @@
  
 <?php include_once('../inc/header.php') ?>
 
-    <section class="page">
-     
-                <div class="page__container">
-                    <div class="row">
-                        <h3>Entry Details</h3>
-                    </div>
-                     
-                    <div class="form" >
-                      <div class="form__field">
-                        <label class="field__label">Name</label>
-                            <label class="checkbox">
-                                <?php echo $data['name'];?>
-                            </label>
-                      </div>
-                      <div class="form__field">
-                        <label class="field__label">Description</label>
-                            <label class="checkbox">
-                                <?php echo $data['description'];?>
-                            </label>
-                      </div>                     
-                      </div>
-                        <div class="form__actions">
-                          <a class="btn" href="../index.php">Back</a>
-                       </div>
-                     
-                      
-                    </div>
+    <section class="page">     
+        <div class="page__container">
+            <div class="row">
+                <h3>Entry Details</h3>
+            </div>                     
+            <div class="form" >
+                <div class="form__field">
+                <label class="field__label">Name</label>
+                    <label class="checkbox">
+                        <?php echo $data['name'];?>
+                    </label>
                 </div>
-            
-            </section>
-                 
-    </div> <!-- /container -->
-  </body>
-</html>
+                <div class="form__field">
+                <label class="field__label">Description</label>
+                    <label class="checkbox">
+                        <?php echo $data['description'];?>
+                    </label>
+                </div>                     
+                </div>
+                <div class="form__actions">
+                    <a class="btn" href="../index.php">Back</a>
+                </div>
+            </div>
+        </div>            
+    </section>     
+                
+<?php include_once('../inc/footer.php') ?>
