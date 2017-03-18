ba002eda13d83e401fb78c29e3496caa0f8c25d4
Yngve Leyn <yngve.leyn@gmail.com>
2017-03-18 20:57:52 +0100

    Established Database connection via PDO

--- index.php
+++ index.php
@@ -18,9 +18,22 @@
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->   
-test
-<button>button!</button>
-        <!-- Add your site or application content here -->
+
+        <?php
+            include 'config/database.php';
+            $pdo = Database::connect();
+            $sql = 'SELECT * FROM entries ORDER BY id DESC';
+           
+                foreach ($pdo->query($sql) as $row) {
+                        echo '<tr>';
+                        echo '<td>'. $row['id'] . '</td>';
+                        echo '<td>'. $row['name'] . '</td>';
+                        echo '<td>'. $row['description'] . '</td>';
+                        echo '</tr>';
+                }
+            
+            Database::disconnect();
+        ?>
      
         <script src="js/app.js"></script>
     </body>
