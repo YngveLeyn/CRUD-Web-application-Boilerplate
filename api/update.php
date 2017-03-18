203dcef8fb01e1e15cf0fde0e1d7becd0a0c6918
Yngve Leyn <yngve.leyn@gmail.com>
2017-03-19 00:15:06 +0100

    Added Update and Delete functionality

---
+++ api/update.php
@@ -0,0 +1,56 @@
+<?php
+    require '../config/database.php';
+ 
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: ../index.php");
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $emailError = null;
+        $mobileError = null;
+         
+        // keep track post values
+        $name = $_POST['name'];
+        $description = $_POST['description'];        
+         
+        // validate input
+        $valid = true;
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
+         
+        if (empty($description)) {
+            $emailError = 'Please enter Email Address';
+            $valid = false;
+        }
+         
+        // update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE entries  set name = ?, description = ? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($name,$description,$id));
+            Database::disconnect();
+            header("Location: ../index.php");
+        }
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM entries where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $name = $data['name'];
+        $description = $data['description'];
+        Database::disconnect();
+    }
+?>
---
+++ pages/update.php
@@ -0,0 +1,36 @@
+<?php include_once('../api/update.php') ?>
+
+<?php include_once('../inc/header.php') ?>
+    <section class="page">     
+        <div class="page__container">
+            <div class="row">
+                <h3>Update an entry</h3>
+            </div>
+        
+            <form class="form" action="update.php?id=<?php echo $id?>" method="post">
+                <div class="form__field <?php echo !empty($nameError)?'error':'';?>">
+                <label class="field__label">Name</label>
+                <div class="controls">
+                    <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                    <?php if (!empty($nameError)): ?>
+                        <span class="input__notification"><?php echo $nameError;?></span>
+                    <?php endif; ?>
+                </div>
+                </div>
+                <div class="form__field <?php echo !empty($descriptionError)?'error':'';?>">
+                <label class="field__label">Description</label>
+                <div class="controls">
+                    <input name="description" type="text" placeholder="Description" value="<?php echo !empty($description)?$description:'';?>">
+                    <?php if (!empty($descriptionError)): ?>
+                        <span class="input__notification"><?php echo $descriptionError;?></span>
+                    <?php endif;?>
+                </div>
+                </div>                    
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-success">Update</button>
+                    <a class="btn" href="../index.php">Back</a>
+                </div>
+            </form>
+        </div>
+    </section>
+<?php include_once('../inc/footer.php') ?>
